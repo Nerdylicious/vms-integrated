1320297f3f2699775453b9c5059dc7e50709544c
Irish Medina <irish.m.medina@gmail.com>
2014-09-25 20:06:28 -0500

    Added an authentication form to be used in login

--- vms/authentication/views.py
+++ vms/authentication/views.py
@@ -1,3 +1,4 @@
+from authentication.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.urlresolvers import reverse
@@ -10,19 +11,25 @@ def index(request):
 def login_process(request):
 
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
 
-        user = authenticate(username=username, password=password)
+        authentication_form = AuthenticationForm(request.POST)
 
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home:index'))
+        if authentication_form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect(reverse('home:index'))
+                else:
+                    return HttpResponse("Your account is disabled.")
             else:
-                return HttpResponse("Your account is disabled.")
+                return render(request, 'authentication/login.html', {'authentication_form' : authentication_form, 'is_invalid_credentials' : True,})
         else:
-            return render(request, 'authentication/login.html', {'is_invalid_credentials' : True,})
+            return render(request, 'authentication/login.html', {'authentication_form' : authentication_form,})
     else:
         return render(request, 'authentication/login.html', {'is_invalid_credentials' : False,})
 
