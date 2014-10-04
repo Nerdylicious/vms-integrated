ce3c2f068b6a31cfa3bb4c40ded7a5d1c3fd85bc
Irish Medina <irish.m.medina@gmail.com>
2014-10-04 00:57:36 -0500

    Added search volunteer form

--- vms/volunteer/forms.py
+++ vms/volunteer/forms.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.forms import ModelForm
 from volunteer.models import Volunteer
 
+class SearchVolunteerForm(forms.Form):
+    first_name = forms.RegexField(regex=r'^[(A-Z)|(a-z)|(\s)]+$', max_length=30, required=False)
+    last_name = forms.RegexField(regex=r'^[(A-Z)|(a-z)|(\s)]+$', max_length=30, required=False)
+    city = forms.RegexField(regex=r'^[(A-Z)|(a-z)|(\s)]+$', max_length=75, required=False)
+    state = forms.RegexField(regex=r'^[(A-Z)|(a-z)|(\s)]+$', max_length=75, required=False)
+    country = forms.RegexField(regex=r'^[(A-Z)|(a-z)|(\s)]+$', max_length=75, required=False)
+    organization = forms.RegexField(regex=r'^[(A-Z)|(a-z)|(\s)]+$', max_length=75, required=False)
+    
 class VolunteerForm(ModelForm):
     class Meta:
         model = Volunteer
