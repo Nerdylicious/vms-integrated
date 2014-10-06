083c704c8938ecd954974a3a5b9f3690cc9fefa3
Irish Medina <irish.m.medina@gmail.com>
2014-10-06 02:46:22 -0500

    Integrated volunteer search functionality

--- vms/volunteer/views.py
+++ vms/volunteer/views.py
@@ -109,4 +109,20 @@ def profile(request, volunteer_id):
 
 @login_required
 def search(request):
-    return render(request, 'volunteer/search.html')
+    if request.method == 'POST':
+        form = SearchVolunteerForm(request.POST)
+        if form.is_valid():
+
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            country = form.cleaned_data['country']
+            organization = form.cleaned_data['organization']
+
+            search_result_list = search_volunteers(first_name, last_name, city, state, country, organization)
+            return render(request, 'volunteer/search.html', {'form' : form, 'has_searched' : True, 'search_result_list' : search_result_list})
+    else:
+        form = SearchVolunteerForm()
+
+    return render(request, 'volunteer/search.html', {'form' : form, 'has_searched' : False})
