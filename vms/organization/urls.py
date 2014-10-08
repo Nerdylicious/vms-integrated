fa690c023ba0d39af0d56249d5b0655487967118
Irish Medina <irish.m.medina@gmail.com>
2014-10-08 02:33:15 -0500

    Created form for creating organizations

--- vms/organization/urls.py
+++ vms/organization/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from organization import views
 
 urlpatterns = patterns('',
+    url(r'^create/$', views.create, name='create'),
     url(r'^list/$', views.list, name='list'),
 )
--- vms/organization/views.py
+++ vms/organization/views.py
@@ -6,6 +6,19 @@ from organization.forms import OrganizationForm
 from organization.services import *
 
 @login_required
+def create(request):
+    if request.method == 'POST':
+        form = OrganizationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('organization:list'))
+        else:
+            return render(request, 'organization/create.html', {'form' : form,})
+    else:
+        form = OrganizationForm()
+        return render(request, 'organization/create.html', {'form' : form,})
+        
+@login_required
 def list(request):
     organization_list = get_organizations_ordered_by_name()
     return render(request, 'organization/list.html', {'organization_list' : organization_list})
