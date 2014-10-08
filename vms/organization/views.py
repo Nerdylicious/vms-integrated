6e274190d9d367f0746c37160b934f51e5d72fbe
Irish Medina <irish.m.medina@gmail.com>
2014-10-08 00:43:54 -0500

    Added organization form

--- vms/organization/views.py
+++ vms/organization/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from organization.forms import OrganizationForm
 
 # Create your views here.
