0032bd07f3e862345b89aaf5e8f3c3bd28edd8ce
Irish Medina <irish.m.medina@gmail.com>
2014-11-03 03:38:20 -0600

    Create template for creating jobs

--- vms/job/views.py
+++ vms/job/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 @login_required
+def create(request):
+    return render(request, 'job/create.html')
+
+@login_required
 def list(request):
     return render(request, 'job/list.html')
