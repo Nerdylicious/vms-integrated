1e5e4e3c67873cd19d9e76efd3a360a529619c21
Irish Medina <irish.m.medina@gmail.com>
2014-11-03 03:18:21 -0600

    Added form validation to check if start date is less than or equal to end date

--- vms/event/forms.py
+++ vms/event/forms.py
@@ -7,3 +7,17 @@ class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = ['name', 'start_date', 'end_date']
+    
+    def clean(self):
+
+        start_date = self.cleaned_data.get('start_date')
+        end_date = self.cleaned_data.get('end_date')
+
+        if start_date > end_date:
+            msg = u"Start date must be before end date"
+            self._errors['start_date'] = self.error_class([msg])
+
+            del self.cleaned_data['start_date']
+            del self.cleaned_data['end_date']
+
+        return self.cleaned_data
