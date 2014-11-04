08e196f9d33c18f848ca30ebd99a0546a11b3b8e
Irish Medina <irish.m.medina@gmail.com>
2014-11-04 03:43:19 -0600

    Fixed validation of start and end dates

--- vms/event/forms.py
+++ vms/event/forms.py
@@ -13,11 +13,9 @@ class EventForm(ModelForm):
         start_date = self.cleaned_data.get('start_date')
         end_date = self.cleaned_data.get('end_date')
 
-        if start_date > end_date:
-            msg = u"Start date must be before end date"
-            self._errors['start_date'] = self.error_class([msg])
-
-            del self.cleaned_data['start_date']
-            del self.cleaned_data['end_date']
+        if start_date and end_date:
+            if start_date > end_date:
+                msg = u"Start date must be before the end date"
+                self._errors['start_date'] = self.error_class([msg])
 
         return self.cleaned_data
