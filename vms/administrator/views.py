ba41bf7cbd66c6ad1ec8f549ab327707293f8704
Irish Medina <irish.m.medina@gmail.com>
2014-11-08 02:08:03 -0600

    Changed administrator view to display organization options by default on settings view

--- vms/administrator/views.py
+++ vms/administrator/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 @login_required
 def settings(request):
-    return render(request, 'administrator/settings.html')
+    return HttpResponseRedirect(reverse('organization:list'))
