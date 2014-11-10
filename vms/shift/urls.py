055b9116cf6c8f3f9f1fd9b960ac781503913864
Irish Medina <irish.m.medina@gmail.com>
2014-11-10 01:41:37 -0600

    Added template that lists volunteer jobs to add shifts to

--- vms/shift/urls.py
+++ vms/shift/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from shift import views
 
 urlpatterns = patterns('',
-    url(r'^list/$', views.list, name='list'),
+    url(r'^create/$', views.create, name='create'),
+    url(r'^list_jobs/$', views.list_jobs, name='list_jobs'),
 )
--- vms/shift/views.py
+++ vms/shift/views.py
@@ -2,7 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from job.services import *
 
 @login_required
-def list(request):
-    return render(request, 'shift/list.html')
+def create(request):
+    return render(request, 'shift/create.html')
+
+@login_required
+def list_jobs(request):
+    job_list = get_jobs_ordered_by_title()
+    return render(request, 'shift/list_jobs.html', {'job_list' : job_list})
