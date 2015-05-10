7af71aa3364693e6e59fd0c0f78645d6cd3bce91
Irish Medina <irish.m.medina@gmail.com>
2015-05-09 22:24:42 -0500

    Moved admin sign up to admin dashboard only

--- vms/registration/views.py
+++ vms/registration/views.py
@@ -1,4 +1,5 @@
 from administrator.forms import AdministratorForm
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
